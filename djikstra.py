14d6fe39e38ddba29b161dc63d4dbece7018dbb3
Theo Reichert <theo.reichert7@gmail.com>
2021-03-08 03:08:32 +0100

    pre test

--- djikstra.py
+++ djikstra.py
@@ -7,35 +7,16 @@ class djikstra_tuple:
         self.cost = cost #integer
         self.predecessor = predecessor #anything
 
-entered = [] #contains djikstra_tuple
-discovered = [] #contains djikstra_tuple
-
-vertexes = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-edges = [("1", 2, "2"), ("1", 4, "6"), ("1", 12, "7"), #(<from_node>, <int>, <to_node>)
-                ("2", 10, "3"), ("2", 9, "7"),
-                ("3", 2, "4"),
-                ("4", 1, "5"),
-                ("6", 11, "5"), ("6", 2, "8"),
-                ("7", 15, "8"),
-                ("8", 9, "5"), ("8", 1, "9"),
-                ("9", 4, "3"), ("9", 7, "4"), ("9", 3, "7")
-                ]
-graph1 = Graph()
-graph1.vertexes = vertexes
-for edge in edges:
-    graph1.edges.append(Edge(edge[0], edge[2], edge[1])) # (from, to, cost)
-
-entered.append(djikstra_tuple("1", 0, "None"))
-
-def pick_shortest():
+def pick_shortest(discovered):
     shortest_path = djikstra_tuple("None", math.inf, "None")
     for path in discovered:
         if path.cost < shortest_path.cost:
             shortest_path = path
     return shortest_path
 
-def update_discovered(shortest_path):
-    for edge in graph1.edges:
+def update_discovered(graph, entered, discovered):
+    shortest_path = entered[-1]
+    for edge in graph.edges:
         A = edge.from
         B = edge.to
         if shortest_path.to == A: #found outgoing edge of newly entered node
@@ -47,3 +28,35 @@ def update_discovered(shortest_path):
                         discovered_path = djikstra_tuple(B, shortest_path.cost+edge.cost, A)
             if not(a_path_to_B_exists):
                 discovered.append(djikstra_tuple(B, shortest_path.cost+edge.cost, A))
+
+def djikstra(node, graph):
+    entered = [] #contains djikstra_tuple
+    discovered = [] #contains djikstra_tuple
+    entered.append(djikstra_tuple(node, 0, "None"))
+    update_discovered(graph, entered, discovered)
+    while len(discovered) > 0:
+        pick_shortest(discovered)
+        update_discovered(graph, entered, discovered)
+    return entered
+
+
+
+############################################################
+vertexes = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+edges = [
+            ("1", 2, "2"), ("1", 4, "6"), ("1", 12, "7"), #(<from_node>, <int>, <to_node>)
+            ("2", 10, "3"), ("2", 9, "7"),
+            ("3", 2, "4"),
+            ("4", 1, "5"),
+            ("6", 11, "5"), ("6", 2, "8"),
+            ("7", 15, "8"),
+            ("8", 9, "5"), ("8", 1, "9"),
+            ("9", 4, "3"), ("9", 7, "4"), ("9", 3, "7")
+        ]
+graph1 = Graph()
+graph1.vertexes = vertexes
+for edge in edges:
+    graph1.edges.append(Edge(edge[0], edge[2], edge[1])) # Edge(from, to, cost)
+
+paths = djikstra("1", graph1)
+print(paths)
