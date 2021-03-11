38123d0ff188a6bf871c65dd4862361388fd2809
Theo Reichert <theo.reichert7@gmail.com>
2021-03-11 13:00:13 +0100

    matrix multiplication untested but should work

--- graph/Matrix.py
+++ graph/Matrix.py
@@ -1,12 +1,24 @@
 class Matrix:
-    def __init__(self, rows, columns):
+    def __init__(self, amount_rows, amount_columns):
         self.row = list()
-        for i in range(rows):
+        for i in range(amount_rows):
             dummy_row = list()
-            for i in range(columns):
+            for i in range(amount_columns):
                 dummy_row.append(0)
             self.row.append(dummy_row)
         self.print()
+    def rows(self, string):
+        if string == "amount": return len(self.row)
+        elif string == "": return self.row
+    def columns(self, string):
+        if string == "amount": return len(self.row[0])
+        elif string == "":
+            columns = list()
+            for column_index in range(len(self.columns("amount"))):
+                column = list()
+                for row in self.row:
+                    column.append(row[column_index])
+                columns.append(column)
     def row(self, i):
         return self.row[i]
     def column(self, i):
@@ -24,3 +36,24 @@ class Matrix:
         # for row in range(len(self.row)):
         #     for column in range(len(self.row[row])):
         #         print(self.entry(row, column))
+    def add(m1, m2):
+        if len(m1.row) == len(m2.row):
+            for row in range(len(m1.row)):
+                if len(m1.row[row]) != m2.row[row]:
+                    return "ERROR"
+        else: return "ERROR"
+        matrix = Matrix(len(m1.row), len(m1.row[0]))
+        for row in range(len(m1.row)):
+            for column in range(len(row)):
+                matrix.enter(m1.row[row][column]+m2.row[row][column], row, column)
+        return matrix
+    def multiply(m1, m2):
+        if m1.columns("amount") != m2.rows("amount"):
+            return "ERROR"
+        matrix = Matrix(m1.rows("amount"), m2.columns("amount"))
+        for i in m2.columns("amount"): # <=> i is a column variable for m2
+            for k in m1.rows("amount"): # <=> k is a row variable for m1
+                sum = 0
+                for j in range(m2.rows("amount")): # or in range(m1.columns) <=> j is a row variabel for m2 and a column variable for m1 
+                    sum += m1.entry(k,j)*m2.entry(j,i)
+                matrix.enter(sum, k, i) #for every column in m2 there will be
